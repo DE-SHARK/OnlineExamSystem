a4cdc9cb8929b4750e4d1b9c1b7460dd772f680f
Mr.Hemispheres <112485017+zudududuyi@users.noreply.github.com>
2023-11-27 19:17:50 +0800

    11.27

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("yt的第一个java代码");
         System.out.println("1");
+        System.out.println("1");
     }
 }
