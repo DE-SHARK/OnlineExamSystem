2a7385ba952845219753a946fc7220e2e28c2cee
DE_SHARK <2799045359@qq.com>
2023-11-27 12:01:56 +0800

    添加 connectMysql.java 可用于连接数据库

---
+++ src/main/java/org/liulinger/jdbc/connectMysql.java
@@ -0,0 +1,18 @@
+package org.liulinger.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class connectMysql {
+    public Connection getConnection(String url, String username, String password) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+//        String url = "jdbc:mysql://129.211.26.208/javaweb";
+//        String username = "root";
+//        String password = "1GVHFVoEp0R98woH8RCG";
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    public connectMysql() {
+    }
+}
