82ee78a3c7be7e75f3d6bb3264dd7a42b95befe4
ddf <CVMaster@geek.com>
2023-12-15 12:22:48 +0800

    新增examservlet注入依赖

--- src/main/java/org/liulinger/controller/ExamListServlet.java
+++ src/main/java/org/liulinger/controller/ExamListServlet.java
@@ -2,29 +2,44 @@ package org.liulinger.controller;
 
 
 import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.liulinger.Bean.ExamBean;
+import org.liulinger.Dao.ExamListDao;
 import org.liulinger.Dao.ExamListDaoImpl;
-
+import org.liulinger.Service.ExamListService;
+import org.liulinger.Service.ExamListServiceImpl;
 
 import java.io.IOException;
 import java.util.List;
 
 @WebServlet("/ExamListServlet")
 public class ExamListServlet extends HttpServlet {
+
+    private ExamListService examListService;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        //注入依赖
+        ExamListDao examListDao = new ExamListDaoImpl();
+        this.examListService = new ExamListServiceImpl(examListDao);
+
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int page = 1;
         int recordsPerPage = 5;
         String stu_id = request.getParameter("stu_id");
         if(request.getParameter("page") != null)
             page = Integer.parseInt(request.getParameter("page"));
-        ExamListDaoImpl examListDao = new ExamListDaoImpl();
-        List<ExamBean> list = examListDao.getUsersPaginated(page, recordsPerPage, stu_id);
-        int noOfRecords = examListDao.getNumberOfExam(stu_id);
+        List<ExamBean> list = examListService.getUsersPaginated(page, recordsPerPage, stu_id);
+        int noOfRecords = examListService.getNumberOfExam(stu_id);
         int noOfPages = (int) Math.ceil(noOfRecords * 1.0 / recordsPerPage);
         request.setAttribute("examList", list);
         request.setAttribute("noOfPages", noOfPages);
