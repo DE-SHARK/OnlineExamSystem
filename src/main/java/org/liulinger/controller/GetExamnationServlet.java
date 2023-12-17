b7d1ace6666728c98e989795b8940ff295310944
wqeqwdc <100198686+wqeqwdc@users.noreply.github.com>
2023-12-17 22:18:52 +0800

    改变了sevlet路径

--- src/main/java/org/liulinger/controller/GetExamnationServlet.java
+++ src/main/java/org/liulinger/controller/GetExamnationServlet.java
@@ -10,7 +10,7 @@ import org.liulinger.Service.Impl.GetExamServiceImpl;
 
 import java.io.IOException;
 
-@WebServlet("/GetExamnationServlet")
+@WebServlet("/student/GetExamnationServlet")
 public class GetExamnationServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
