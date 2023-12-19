1cae3208cfc9354d99a4a6244bc166753f371ec4
DE_SHARK <2799045359@qq.com>
2023-12-19 16:06:25 +0800

    新增controller.admin.itemAdd.generate包

--- src/main/java/org/liulinger/controller/admin/itemAdd/generate/GenerateTeachers.java
+++ src/main/java/org/liulinger/controller/admin/itemAdd/generate/GenerateTeachers.java
@@ -1,4 +1,4 @@
-package org.liulinger.controller.admin.itemAdd;
+package org.liulinger.controller.admin.itemAdd.generate;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletConfig;
